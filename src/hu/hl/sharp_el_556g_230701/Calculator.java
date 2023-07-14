ddccb5b96e4bb51d6ddffcb74af05ef34dff6b87
Horváth László <la1260@freemail.hu>
2023-07-14 16:36:23 +0200

    230714 Stacklistener megszüntetve, x+? működik, a cseréje még nem.

--- src/hu/hl/sharp_el_556g_230701/Calculator.java
+++ src/hu/hl/sharp_el_556g_230701/Calculator.java
@@ -1,39 +1,56 @@
 package hu.hl.sharp_el_556g_230701;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
-public class Calculator implements InputListener, StackListener {
+import hu.hl.sharp_el_556g_230701.Main.Base;
+import hu.hl.sharp_el_556g_230701.Main.Fse;
+
+public class Calculator implements InputListener {
 	private final CalculatorListener calculatorlistener;
-	private Input input;
-	public Stack stack= new Stack(this);
+	public Stack stack= new Stack();
 	public Calculator(CalculatorListener calculatorlistener) {
 		this.calculatorlistener= calculatorlistener;
-		reset();
+		key(16);
 	}
+	private Input input;
 	private BigDecimal result;
-	private void reset() {
-		result= null;
-		stack.reset();
-		if (input!=null) input.reset();
-	}
+	private int prevopcode;
+	private BigDecimal previnput;
 	public void key(int keycode) {
 		if (-1<keycode)	{
-			if (keycode<21) {
-				if (keycode==16) {
-					reset();
+			if (keycode<21 && keycode!=16) {
+				if (keycode==19 && input==null) {
+					prevopcode= stack.getLastOperator();
 				} else {
 					if (input==null) input= new Input(this);
 					input.key(keycode);
 				}
 			} else {
-				if (input!=null) {
-					result= stack.push(input.getValue(), keycode);
+				if (keycode==16) {
+					stack.reset();
+					prevopcode= -1;
+					result= stack.push(BigDecimal.ZERO, 21);
+				} else if (keycode==21 && -1<prevopcode) {
+					if (input!=null) {
+						stack.reset();
+						result= stack.push(input.getValue(), prevopcode);
+					}
+					result= stack.push(previnput, 21);
+					stack.push(result, prevopcode);
+				} else if (input!=null) {
+					result= stack.push(previnput= input.getValue(), keycode);
 				} else if (result!=null && keycode!=26) {
 					result= stack.push(result, keycode);
 				} else {
 					result= stack.push(null, keycode);
 				}
 				input= null;
+				
+				String[] s= (result==null) ? new String[]{"ide kell vmi", ""} : print(result, Base.Dec, Fse.Nrm, 4);
+				update("", s[0], s[1]);
 			}
 			System.out.println(stack.stack0);
 			System.out.println(stack.stack1);
@@ -42,6 +59,75 @@ public class Calculator implements InputListener, StackListener {
 	public void update(String s0, String s1, String s2) {
 		calculatorlistener.onChange(s0, s1, s2);
 	}
+	
+	
+	
+	private static DecimalFormat df= new DecimalFormat();
+    static {
+		DecimalFormatSymbols dfs= new DecimalFormatSymbols();
+		dfs.setDecimalSeparator('.');
+		df.setDecimalFormatSymbols(dfs);    	
+		df.setGroupingUsed(false);
+		df.setRoundingMode(RoundingMode.HALF_UP);
+    }
+	private String format(BigDecimal bd, String pattern) {
+		df.applyPattern(pattern);
+		df.setDecimalSeparatorAlwaysShown(true);
+		return df.format(bd);
+	}
+	private String[] print(BigDecimal bd, Base base, Fse fse, int tab) {
+		String[] result= {null, null};
+		switch (base) {
+		case Bin:
+			result[0]= bd.toBigInteger().toString(2);
+			result[1]= "b";
+			break;
+		case Oct:
+			result[0]= bd.toBigInteger().toString(8);
+			result[1]= "o";
+			break;
+		case Dec:
+			int exp= bd.precision()-bd.scale()-1;
+			switch (fse) {
+			case Nrm:
+				if (-10<exp && exp<10) {
+					result[0]= format(bd, "#########0.#########".substring(Integer.min(9, 9-exp), Integer.min(20, 20-exp)));
+					result[1]= "";
+				} else {
+					return print(bd, base, Fse.Sci, -1);
+				}
+				break;
+			case Fix:
+				if (10<=exp) {
+					return print(bd, base, Fse.Sci, tab);
+				} else {
+					if (exp<=-10) {
+						bd= BigDecimal.ZERO;
+						exp= bd.precision()-bd.scale()-1;
+					}
+					result[0]= format(bd, "#########0.000000000".substring(Integer.min(9, 9-exp), Integer.min(11+tab, 20-exp)));
+					result[1]= "";
+				}
+				break;
+			case Sci:
+				if (tab==-1) {
+					result= format(bd, "0.#########E00").split("E");
+				} else {
+					result= format(bd, "0."+"000000000".substring(0, tab)+"E00").split("E");
+				}
+				break;
+			case Eng:
+				result= format(bd, "000.000000000".substring(Math.floorMod(2-exp, 3), tab+4)+"E00").split("E");
+				break;
+			}
+			break;
+		case Hex:
+			result[0]= bd.toBigInteger().toString(16);
+			result[1]= "h";
+			break;
+		}
+		return result;
+	}	
 }
 
 interface CalculatorListener {
--- src/hu/hl/sharp_el_556g_230701/Stack.java
+++ src/hu/hl/sharp_el_556g_230701/Stack.java
@@ -10,19 +10,14 @@ import hu.hl.sharp_el_556g_230701.Main.Base;
 import hu.hl.sharp_el_556g_230701.Main.Fse;
 
 public class Stack {
-	private final StackListener stacklistener;
 	private static final MathContext mathcontext= new MathContext(18, RoundingMode.HALF_UP);
 	public java.util.Stack<BigDecimal> stack0= new java.util.Stack<BigDecimal>();
 	public java.util.Stack<Integer> stack1= new java.util.Stack<Integer>();
 	private int pl;
-	public Stack(StackListener stacklistener) {
-		this.stacklistener= stacklistener;
-	}
 	public void reset() {
 		stack0.clear();
 		stack1.clear();
 		pl= 0;
-		push(BigDecimal.ZERO, 21); 
 	}
 	public BigDecimal push(BigDecimal operand, int operator) {
 		BigDecimal result= null;
@@ -34,23 +29,13 @@ public class Stack {
 			pl= 0;
 			break;
 		case 22: // +
+		case 23: // -
 			if (operand!=null) stack0.push(operand);
 			if (stack0.size()==stack1.size()) stack1.pop(); //kiszedi az előző műveletet, ha újat nyomtunk operandus nélkül
 			while ((pl<<8 | 22)<=getLastOperator()) executeLastOperator();
 			stack1.push(pl<<8 | operator);
 			break;
-		case 23: // -
-			if (operand!=null) stack0.push(operand);
-			if (stack0.size()==stack1.size()) stack1.pop();
-			while ((pl<<8 | 22)<=getLastOperator()) executeLastOperator();
-			stack1.push(pl<<8 | operator);
-			break;
 		case 24: // *
-			if (operand!=null) stack0.push(operand);
-			if (stack0.size()==stack1.size()) stack1.pop();
-			while ((pl<<8 | 24)<=getLastOperator()) executeLastOperator();
-			stack1.push(pl<<8 | operator);
-			break;
 		case 25: // /
 			if (operand!=null) stack0.push(operand);
 			if (stack0.size()==stack1.size()) stack1.pop();
@@ -76,11 +61,11 @@ public class Stack {
 			}
 			break;
 		}
-		String[] s= print((stack0.isEmpty()) ? result : stack0.peek(), Base.Dec, Fse.Nrm, 4);
-		stacklistener.update("", s[0], s[1]);
+//		String[] s= print((stack0.isEmpty()) ? result : stack0.peek(), Base.Dec, Fse.Nrm, 4);
+//		stacklistener.update("", s[0], s[1]);
 		return result;
 	}
-	private int getLastOperator() {
+	public int getLastOperator() {
 		return (stack1.isEmpty()) ? -1 : stack1.peek();
 	}
 	private boolean executeLastOperator() {
@@ -98,76 +83,4 @@ public class Stack {
 		}
 		return !stack1.isEmpty();
 	}
-	
-	
-	private static DecimalFormat df= new DecimalFormat();
-    static {
-		DecimalFormatSymbols dfs= new DecimalFormatSymbols();
-		dfs.setDecimalSeparator('.');
-		df.setDecimalFormatSymbols(dfs);    	
-		df.setGroupingUsed(false);
-		df.setRoundingMode(RoundingMode.HALF_UP);
-    }
-	private String format(BigDecimal bd, String pattern) {
-		df.applyPattern(pattern);
-		df.setDecimalSeparatorAlwaysShown(true);
-		return df.format(bd);
-	}
-	private String[] print(BigDecimal bd, Base base, Fse fse, int tab) {
-		String[] result= {null, null};
-		switch (base) {
-		case Bin:
-			result[0]= bd.toBigInteger().toString(2);
-			result[1]= "b";
-			break;
-		case Oct:
-			result[0]= bd.toBigInteger().toString(8);
-			result[1]= "o";
-			break;
-		case Dec:
-			int exp= bd.precision()-bd.scale()-1;
-			switch (fse) {
-			case Nrm:
-				if (-10<exp && exp<10) {
-					result[0]= format(bd, "#########0.#########".substring(Integer.min(9, 9-exp), Integer.min(20, 20-exp)));
-					result[1]= "";
-				} else {
-					return print(bd, base, Fse.Sci, -1);
-				}
-				break;
-			case Fix:
-				if (10<=exp) {
-					return print(bd, base, Fse.Sci, tab);
-				} else {
-					if (exp<=-10) {
-						bd= BigDecimal.ZERO;
-						exp= bd.precision()-bd.scale()-1;
-					}
-					result[0]= format(bd, "#########0.000000000".substring(Integer.min(9, 9-exp), Integer.min(11+tab, 20-exp)));
-					result[1]= "";
-				}
-				break;
-			case Sci:
-				if (tab==-1) {
-					result= format(bd, "0.#########E00").split("E");
-				} else {
-					result= format(bd, "0."+"000000000".substring(0, tab)+"E00").split("E");
-				}
-				break;
-			case Eng:
-				result= format(bd, "000.000000000".substring(Math.floorMod(2-exp, 3), tab+4)+"E00").split("E");
-				break;
-			}
-			break;
-		case Hex:
-			result[0]= bd.toBigInteger().toString(16);
-			result[1]= "h";
-			break;
-		}
-		return result;
-	}	
-}
-
-interface StackListener {
-	public void update(String s0, String s1, String s2);
 }
