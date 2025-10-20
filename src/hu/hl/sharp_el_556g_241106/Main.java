90e6df0be188919b6d7ceaf99dd48b1a6ed62d71
Horváth László <la1260@freemail.hu>
2025-10-20 08:22:12 +0200

    Input 2ndf nélkül.

--- src/hu/hl/sharp_el_556g_241106/Main.java
+++ src/hu/hl/sharp_el_556g_241106/Main.java
@@ -48,43 +48,107 @@ public class Main {
 			Input.addBack(in);
 			break;
 		case 's':
-			Input.addExp(in);
+			Input.addExp(in, isDec());
 			break;
 		case 't':
-			Input.addSlash(in);
+			Input.addSlash(in, isDec());
 			break;
 		case 'u':
-			Input.addDegree(in);
+			Input.addDegree(in, isDec());
 			break;
 		case 'y':
-			Input.addNeg(in);
+			Input.addNeg(in, isDec());
 			break;
-		case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
-			Input.addNum(in, c);
+		case 'm':
+			if (isHex()) {
+				Input.addNum(in, 'A');
+			}
+			break;
+		case 'n':
+			if (isHex()) {
+				Input.addNum(in, 'B');
+			}
+			break;
+		case 'o':
+			if (isHex()) {
+				Input.addNum(in, 'C');
+			}
+			break;
+		case 'p':
+			if (isHex()) {
+				Input.addNum(in, 'D');
+			}
+			break;
+		case 'q':
+			if (isHex()) {
+				Input.addNum(in, 'E');
+			}
+			break;
+		case 'r':
+			if (isHex()) {
+				Input.addNum(in, 'F');
+			}
+			break;
+		case '0': case '1':
+			if (isDec() || isBin() || isOct() || isHex()) {
+				Input.addNum(in, c);
+			}
+			break;
+		case '2': case '3': case '4': case '5': case '6': case '7':
+			if (isDec() || isOct() || isHex()) {
+				Input.addNum(in, c);
+			}
+			break;
+		case '8': case '9':
+			if (isDec() || isHex()) {
+				Input.addNum(in, c);
+			}
 			break;
 		case '.':
-			Input.addDot(in);
+			Input.addDot(in, isDec());
 			break;
-		}		
-		String[] ss= Output.getDisplay(in);
+		}
+		String[] ss= Output.getDisplay(in, isDec(), isBin(), isOct(), isHex());
 		ss[3]= String.format("%s,%s,", calculator.getMode0(), calculator.getMode1());
 		form.update(ss);
+	}
+	private boolean isDec() {
+		return true;
+	}
+	private boolean isBin() {
+		return false;
+	}
+	private boolean isOct() {
+		return false;
+	}
+	private boolean isHex() {
+		return false;
 	}
 }
 
 class Output {
-	public static String[] getDisplay(StringBuilder sb) {
+	public static String[] getDisplay(StringBuilder sb, boolean isDec, boolean isBin, boolean isOct, boolean isHex) {
 		String[] result= new String[7];
-		if (Input.isUres(sb.toString())) {
-			result[1]= "0";
-		} else {
+		if (isDec) {
 			if (Input.isExp(sb.toString())) {
 				result[1]= String.format("%s", sb.toString().split("e")[0]);
 				result[2]= String.format("%s", sb.toString().split("e")[1]);
 			} else {
 				result[1]= String.format("%s", sb.toString());
-			}
+			} 
+		} else if (isBin) {
+			result[1]= String.format("%s", sb.toString());
+			result[2]= String.format("b");
+		} else if (isOct) {
+			result[1]= String.format("%s", sb.toString());
+			result[2]= String.format("o");
+		} else if (isHex) {
+			result[1]= String.format("%s", sb.toString());
+			result[2]= String.format("h");
 		}
+		if (Input.isUres(sb.toString())) {
+			result[1]= "0";
+		}		
 		return result;
 	}
 }
@@ -110,32 +174,34 @@ class Input {
 			sb.append("0");
 		}
 	}
-	public static void addSlash(StringBuilder sb) {
-		if (isPerMehet(sb.toString())) {
+	public static void addSlash(StringBuilder sb, boolean isDec) {
+		if (isPerMehet(sb.toString(), isDec)) {
 			sb.append("/");
 		}
 	}
-	public static void addExp(StringBuilder sb) {
-		if (isExpMehet(sb.toString())) {
+	public static void addExp(StringBuilder sb, boolean isDec) {
+		if (isExpMehet(sb.toString(), isDec)) {
 			if (isUres(sb.toString())) {
 				addNum(sb, '1');
 			}
 			sb.append("e00");
 		}
 	}
-	public static void addNeg(StringBuilder sb) {
-		if (isExp(sb.toString())) {
-			if (isNegativ(sb.toString())) {
-				sb.deleteCharAt(sb.indexOf("e")+1);
-			} else {
-				sb.insert(sb.indexOf("e")+1, "-");
-			}
-		} else {
-			if (!isUres(sb.toString())) {
+	public static void addNeg(StringBuilder sb, boolean isDec) {
+		if (isDec) {
+			if (isExp(sb.toString())) {
 				if (isNegativ(sb.toString())) {
-					sb.deleteCharAt(0);
+					sb.deleteCharAt(sb.indexOf("e")+1);
 				} else {
-					sb.insert(0, "-");
+					sb.insert(sb.indexOf("e")+1, "-");
+				}
+			} else {
+				if (!isUres(sb.toString())) {
+					if (isNegativ(sb.toString())) {
+						sb.deleteCharAt(0);
+					} else {
+						sb.insert(0, "-");
+					}
 				}
 			}
 		}
@@ -151,37 +217,39 @@ class Input {
 			sb.append(c);
 		}
 	}
-	public static void addDot(StringBuilder sb) {
+	public static void addDot(StringBuilder sb, boolean isDec) {
 		if (isUres(sb.toString())) {
 			sb.append("0");
 		}
-		if (isPontMehet(sb.toString())) {
+		if (isPontMehet(sb.toString(), isDec)) {
 			sb.append(".");
 		}
 	}
-	public static void addDegree(StringBuilder sb) {
-		if (isFokMehet(sb.toString())) {
+	public static void addDegree(StringBuilder sb, boolean isDec) {
+		if (isFokMehet(sb.toString(), isDec)) {
 			sb.append("°00");
-		} else if (isFokig(sb.toString())) {
-			sb.append("'00");
-		} else if (isPercig(sb.toString())) {
-			sb.append(".00");
+		} else if (isFok(sb.toString())) { 
+			if (isFokig(sb.toString())) {
+				sb.append("'00");
+			} else if (isPercig(sb.toString())) {
+				sb.append(".00");
+			}
 		}
 	}
-	private static boolean isPerMehet(String s) {
-		return s.matches("^-?\\d{1,3}(\\d{0,3}|/\\d{1,3})$") && !isNulla(s);
+	private static boolean isPerMehet(String s, boolean isDec) {
+		return s.matches("^-?\\d{1,3}(\\d{0,3}|/\\d{1,3})$") && !isNulla(s) && isDec;
 	}
 	private static boolean isSzamMehet(String s) {
-		return s.matches("^-?(\\d{0,9}|[\\d\\.]{2,10})$") || s.matches("(?=^-?.{2,9}$)^.*/\\d{0,2}$") || isExp(s) || isFok(s);
+		return s.matches("^-?([\\d,A-F]{0,9}|[\\d\\.]{2,10})$") || s.matches("(?=^-?.{2,9}$)^.*/\\d{0,2}$") || isExp(s) || isFok(s);
 	}
-	private static boolean isPontMehet(String s) {
-		return !isPont(s) && !isPer(s) && !isExp(s) && !isFok(s);
+	private static boolean isPontMehet(String s, boolean isDec) {
+		return !isPont(s) && !isPer(s) && !isExp(s) && !isFok(s) && isDec;
 	}
-	private static boolean isExpMehet(String s) {
-		return !isExp(s) && !isPer(s) && !isFok(s);
+	private static boolean isExpMehet(String s, boolean isDec) {
+		return !isExp(s) && !isPer(s) && !isFok(s) && isDec;
 	}
-	private static boolean isFokMehet(String s) {
-		return !isFok(s) && !isExp(s) && !isPer(s) && !isPont(s) && !isUres(s) && s.matches("^-?\\d{0,4}$");
+	private static boolean isFokMehet(String s, boolean isDec) {
+		return !isFok(s) && !isExp(s) && !isPer(s) && !isPont(s) && !isUres(s) && s.matches("^-?\\d{0,4}$") && isDec;
 	}
 	public static boolean isUres(String s) {
 		return s.isEmpty();
@@ -211,10 +279,10 @@ class Input {
 		return s.contains("°");
 	}
 	private static boolean isFokig(String s) {
-		return isFok(s) && s.charAt(s.length()-3)=='°';
+		return s.charAt(s.length()-3)=='°';
 	}
 	private static boolean isPercig(String s) {
-		return isFok(s) && s.charAt(s.length()-3)=='\'';
+		return s.charAt(s.length()-3)=='\'';
 	}
 }
 
